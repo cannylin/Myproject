55407d4c57fdf7bdd4d36e67ad62c51fcdd49ac2
cannylin <44308831+cannylin@users.noreply.github.com>
2018-12-03 21:35:17 +0800

    1.SchoolData.add

---
+++ XFSampleApp/XFSampleApp/Models/PLC_CMD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFSampleApp.Models
+{
+    public class PLC_Command
+    {
+        public string D0_D9 { get; set; }
+        public string D10_D19 { get; set; }
+        public string D20_D29 { get; set; }
+
+        public string D30_D39 { get; set; }
+        public string D40_D49 { get; set; }
+    }
+}
--- XFSampleApp/XFSampleApp/Pages/MainPage.xaml.cs
+++ XFSampleApp/XFSampleApp/Pages/MainPage.xaml.cs
@@ -6,24 +6,22 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using System.Collections.ObjectModel;
 using XFSampleApp.Models;
+using System.Net;         //cannylin
+using System.Net.Sockets;  //cannylin
 
 namespace XFSampleApp.Pages
 {
 
 
-    public class My_BIND_DATA
-    {
-        public string Name { get; set; }
-        public string Location { get; set; }
-        public string Details { get; set; }
-        public string ImageUrl { get; set; }
-        public bool? IsMischievous { get; set; }
-    }
+
+    
     public partial class MainPage : ContentPage
 
     {
         ObservableCollection <string> schoolData2; //宣告變數 SCHOOLDATA 的抽屜放STRING
         ObservableCollection<SchoolINFO> schoolData; //宣告變數 SCHOOLDATA 的抽屜放STRING
+         int PLC_connect_ststus { get;  set; }
+        String REC_PLC { get;  set; }
 
 
         public MainPage()
@@ -33,25 +31,36 @@ namespace XFSampleApp.Pages
             BuildSchoolData();
             SchoolListView.ItemsSource = schoolData;
             
-
-
             // xaml Ui 元件本身 一定會有 "繫結"
         }
-
-        private  void BuildSchoolData()
+        private void BuildSchoolData()
         {
             schoolData = new ObservableCollection<SchoolINFO>()
           {
-           new SchoolINFO() { Name = "NAME_AAAAA", LOGO = "auo",ADDRESS="ADD111111",TEL="111111",Email="Email_11111"},
-           new SchoolINFO() { Name = "NAME_BBBBB", LOGO = "auo",ADDRESS="ADD2222222",TEL="22222",Email="Email_11111"},
-           new SchoolINFO() { Name = "NAME_ccccc", LOGO = "googlelogo",ADDRESS="ADD3333",TEL="3333",Email="Email_11111"},
-           new SchoolINFO() { Name = "NAME_dddd", LOGO = "googlelogo",ADDRESS="ADD4444",TEL="44444",Email="Email_11111" },
+           new SchoolINFO() { Name = "NAME_AAAAA", LOGO = "auo",ADDRESS="ADD111111",TEL="111111",Email="Email_11111@gmail.com"},
+           new SchoolINFO() { Name = "NAME_BBBBB", LOGO = "auo",ADDRESS="ADD2222222",TEL="22222",Email="Email222222@gmail.com"},
+           new SchoolINFO() { Name = "NAME_ccccc", LOGO = "googlelogo",ADDRESS="ADD3333",TEL="3333",Email="Email333333@gmail.com"},
+           new SchoolINFO() { Name = "NAME_dddd", LOGO = "googlelogo",ADDRESS="ADD4444",TEL="44444",Email="Emai44444@gmail.com" },
            };
+            
+            schoolData.Add(new SchoolINFO
+            {
+                Name = "ADDName555555",
+                LOGO = "auo",
+                ADDRESS = "ADD66666666",
+                TEL = "7777777777777",
+                Email = "Email_88888@gmail.com"
+            }
+              
+            );
+
+
         }
-      //  private async void Button_Clicked(object sender, EventArgs e)
-       // {
-      //     await Navigation.PushAsync(new DetailPage());
-       // }
+
+        //  private async void Button_Clicked(object sender, EventArgs e)
+        // {
+        //     await Navigation.PushAsync(new DetailPage());
+        // }
 
         private async void ListView_Refreshing(object sender, EventArgs e)
         {
@@ -66,6 +75,25 @@ namespace XFSampleApp.Pages
             await Navigation.PushAsync(new DetailPage(SchoolINFO));
             SchoolListView.SelectedItem = null; //點選反回無標記
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         private void BT_click()
         {
@@ -91,5 +119,124 @@ namespace XFSampleApp.Pages
             // sms 
             //app link行為 正確的URI格式需正確
         }
+
+        private void Mail_BT_CLICK(object sender, EventArgs e)
+        {
+            var Mail_BT = sender as Button;
+            var Mail_ID = Mail_BT.BindingContext as SchoolINFO;
+            var Mail_To = Mail_ID.Email;
+            Device.OpenUri(new Uri($"mailto:{Mail_To}"));
+
+           // Device.OpenUri(new Uri("mailto:ryan.hatfield@test.com"));
+
+        }
+
+        public void PLC_Connect()
+        {
+
+
+            /*建立以TCP為協定的Socket*/
+            Socket m_SocketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //   Console.Write("請輸入遠端IP位置 : ");
+            //   string m_RemoteIP = Console.ReadLine();
+            string m_RemoteIP = "192.168.31.31";
+
+            IPAddress m_RemoteAddress;
+
+
+
+            while (true)
+            {
+                /*離開程式*/
+                // if (PLC_connect_ststus == 0)
+                //    return;
+                /*將IP字串轉換成IPAddress物件*/
+                if (!IPAddress.TryParse(m_RemoteIP, out m_RemoteAddress))
+                {
+
+                    DisplayAlert("IP位置格式錯誤", "檢查IP", "再試試");
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+            /*初始化IPEndPoint物件,表示要連結的遠端IP位置和Port號4999/PLC_1378*/
+            IPEndPoint m_IPPoint = new IPEndPoint(m_RemoteAddress, 4999);
+            try
+            {
+                /*連結遠端Socket*/
+                m_SocketClient.Connect(m_IPPoint);
+                /*接收資料的緩衝區*/
+                byte[] m_RecvBuffer = new byte[8192];
+                /*送出資料的緩衝區*/
+                byte[] m_SendBuffer;
+                int m_RecvLen;
+                string m_SendMessage;
+                /*接收伺服器login歡迎訊息*/
+                //     m_RecvLen = m_SocketClient.Receive(m_RecvBuffer, SocketFlags.None);
+                //     ShowMessage(m_RecvBuffer, m_RecvLen);
+
+
+                // Console.WriteLine("A333333333333___PLC_connect_ststus="+ PLC_connect_ststus);
+
+                //     while (PLC_connect_ststus ==1)
+                //    {
+                //   Console.Write("輸入訊息 : ");
+                /*輸入運算的資訊*/
+                //  m_SendMessage = Console.ReadLine();
+                m_SendMessage = "01FF000A4420000000000500";
+                /*編碼要傳出的訊息*/
+                m_SendBuffer = Encoding.Default.GetBytes(m_SendMessage);
+                /*送出訊息到伺服器*/
+                m_SocketClient.Send(m_SendBuffer, m_SendBuffer.Length, SocketFlags.None);
+                /*如果送出的訊息是exit則跳出*/
+                //   if (m_SendMessage == "exit")
+                //       break;
+                /*接收伺服器回傳的訊息*/
+
+
+                m_RecvLen = m_SocketClient.Receive(m_RecvBuffer, SocketFlags.None);
+
+
+
+
+                ShowMessage(m_RecvBuffer, m_RecvLen);
+                // }
+                /*關閉連接,確保已連接通訊端上的所有資料在關閉連接前都會加以傳送和接收*/
+                m_SocketClient.Shutdown(SocketShutdown.Both);
+                /*關閉 Socket 連接並釋放所有相關資源*/
+                m_SocketClient.Close();
+                //  Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                m_SocketClient.Close();
+                Console.WriteLine(string.Format("錯誤發生 Exception ex : {0}", ex.Message));
+                Console.ReadLine();
+            }
+
+            /*解碼並顯示伺服器傳回的訊息*/
+            void ShowMessage(byte[] buffer, int len)
+            {
+
+                //  DisplayAlert("接收字串=", Encoding.Default.GetString(buffer, 0, len), "");
+
+                REC_PLC = Encoding.Default.GetString(buffer, 0, len);
+
+
+                DisplayAlert("接收字串=====", REC_PLC, "ok");
+
+                Console.WriteLine("伺服器訊息111111111 : {0}", REC_PLC);
+
+                Console.WriteLine("伺服器訊息 : {0}", Encoding.Default.GetString(buffer, 0, len));
+
+
+            }
+
+
+        }
+
     }
 }
